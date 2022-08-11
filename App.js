359fed71393c9370ae6e90250400b2884b843a6f
romadebrian <roma_coll04@yahoo.com>
2022-08-11 18:47:50 +0700

    React Navigation

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/page/Home/Home';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
